fba1bd4f8849523c2bdcf97c904ad5dbd030fbf9
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-01-22 11:39:32 +0900

    Set user-agent

--- accounting/mf_crawler.py
+++ accounting/mf_crawler.py
@@ -1,6 +1,7 @@
 import os
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 
 
@@ -14,14 +15,22 @@ def mf_driver():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+    )
     driver = webdriver.Chrome(options=options)
-    driver.get("https://id.moneyforward.com/sign_in/email")
-    driver.find_element_by_name("mfid_user[email]").send_keys(os.environ["MF_EMAIL"])
-    driver.find_element_by_xpath("//input[@type='submit' and @value='同意してログインする']").click()
-    driver.find_element_by_name("mfid_user[password]").send_keys(os.environ["MF_PASSWORD"])
-    driver.find_element_by_xpath("//input[@type='submit' and @value='ログインする']").click()
-    driver.get("https://moneyforward.com")
-    driver.find_element_by_xpath("//input[@type='submit' and @value='このアカウントを使用する']").click()
+    try:
+        driver.get("https://id.moneyforward.com/sign_in/email")
+        driver.find_element_by_name("mfid_user[email]").send_keys(os.environ["MF_EMAIL"])
+        driver.find_element_by_xpath("//input[@type='submit' and @value='同意してログインする']").click()
+        driver.find_element_by_name("mfid_user[password]").send_keys(os.environ["MF_PASSWORD"])
+        driver.find_element_by_xpath("//input[@type='submit' and @value='ログインする']").click()
+        driver.get("https://moneyforward.com/sign_in")
+        driver.find_element_by_xpath("//input[@type='submit' and @value='このアカウントを使用する']").click()
+    except NoSuchElementException as e:
+        print(driver.page_source)
+        raise e
     return driver
 
 
