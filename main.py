e792083406ed022e7c444870e82eb38b1648e696
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-01-23 17:38:07 +0900

    Send balance sheet to LINE

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import datetime
+
+from accounting.line_notification import notify
 from accounting.mf_crawler import get_balance_sheet
 
-print(get_balance_sheet())
+
+def main():
+    """Send blance sheet to LINE."""
+    bs = get_balance_sheet()
+    notify(format_bs(bs))
+
+
+def format_bs(bs):
+    """Format balance sheet dict.
+
+    Args:
+        bs (dict): keys are total_asset, liability, and net_asset.
+
+    Returns:
+        str: formatted balance sheet with date.
+
+    """
+    today = datetime.datetime.today().strftime("%Y-%m-%d")
+    bs_values = "\n".join(bs.values())
+    return f"{today}\n{bs_values}"
+
+
+if __name__ == "__main__":
+    main()
