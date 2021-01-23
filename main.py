a64a475de4f94db736d3937f3f38a7adc641015e
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-01-23 22:13:06 +0900

    Store bs data in firestore

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+from accounting.firebase import store_bs
 from accounting.formatter import format_bs
 from accounting.line_notification import notify
 from accounting.mf_crawler import get_balance_sheet
@@ -6,6 +10,12 @@ from accounting.mf_crawler import get_balance_sheet
 def main():
     """Send blance sheet to LINE."""
     bs = get_balance_sheet()
+
+    if os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") is not None:
+        store_bs(bs)
+    else:
+        sys.stderr("GOOGLE_APPLICATION_CREDENTIAL doesn't exist. Skipped setting firestore.")
+
     notify(format_bs(bs))
 
 
