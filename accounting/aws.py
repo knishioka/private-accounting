c7b8e14a5dd2535c447fa709d8890ca9be93b23c
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-02-15 20:00:58 +0900

    Add aws.py to decrypt cipher text by kms

---
+++ accounting/aws.py
@@ -0,0 +1,17 @@
+import base64
+
+import boto3
+
+
+def kms_decrypt(encrypted_txt):
+    """Decrypt cipher text.
+
+    Args:
+        encrypted_txt (str): text encrypted by kms.
+
+    Returns:
+        str: decrypted cipher text.
+
+    """
+    client = boto3.client("kms")
+    return client.decrypt(CiphertextBlob=base64.b64decode(encrypted_txt))["Plaintext"]
