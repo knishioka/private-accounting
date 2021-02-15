e299a7014a0a17bf0667231155ecabc475937883
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-02-15 20:11:12 +0900

    Use encrypted line token

--- accounting/line_notification.py
+++ accounting/line_notification.py
@@ -2,6 +2,8 @@ import os
 
 import requests
 
+from .aws import kms_decrypt
+
 
 def notify(message):
     """Notify to line.
@@ -10,7 +12,10 @@ def notify(message):
         message (str): message for notification.
 
     """
-    line_token = os.environ["LINE_TOKEN"]
+    if encrypted_line_token := os.environ.get("ENCRYPTED_LINE_TOKEN"):
+        line_token = kms_decrypt(encrypted_line_token)
+    else:
+        line_token = os.environ["LINE_TOKEN"]
     line_api_url = "https://notify-api.line.me/api/notify"
     headers = {"Authorization": f"Bearer {line_token}"}
     data = {"message": f"message: {message}"}
