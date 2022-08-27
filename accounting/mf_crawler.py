b2a7853a98dbf4cf3b400998e3f2aa2afca7642e
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2022-08-27 10:09:29 -1000

    Use data path for headless chrome

--- accounting/mf_crawler.py
+++ accounting/mf_crawler.py
@@ -1,4 +1,5 @@
 import os
+from tempfile import mkdtemp
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -15,27 +16,21 @@ def mf_driver():
 
     """
     options = Options()
-    options.binary_location = "/tmp/bin/headless-chromium"
+    options.binary_location = "/opt/chrome/chrome"
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    options.add_argument("--single-process")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1280x1696")
-    options.add_argument("--disable-application-cache")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--hide-scrollbars")
-    options.add_argument("--enable-logging")
-    options.add_argument("--log-level=0")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--homedir=/tmp")
-    options.add_argument("--enable-javascript")
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
-    )
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
 
-    driver = webdriver.Chrome(executable_path="/tmp/bin/chromedriver", chrome_options=options)
+    driver = webdriver.Chrome("/opt/chromedriver", chrome_options=options)
     try:
         driver.get("https://id.moneyforward.com/sign_in/email")
         driver.find_element_by_name("mfid_user[email]").send_keys(email())
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import sys
 
 from accounting.firebase import store_bs
@@ -8,18 +7,8 @@ from accounting.line_notification import notify
 from accounting.mf_crawler import get_balance_sheet
 
 
-def move_bin(fname: str, src_dir: str = "/var/task/bin", dest_dir: str = "/tmp/bin") -> None:
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-    dest_file = os.path.join(dest_dir, fname)
-    shutil.copy2(os.path.join(src_dir, fname), dest_file)
-    os.chmod(dest_file, 0o775)
-
-
 def handler(event, context):
     """Send blance sheet to LINE."""
-    move_bin("headless-chromium")
-    move_bin("chromedriver")
 
     bs = get_balance_sheet()
 
